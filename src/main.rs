ecbdc82882be8514ecd39d2050a4a51dac4e3ca7
Cody Taylor <codemister99@yahoo.com>
2023-12-01 07:08:29 -0500

    Task 1-1 complete.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use rocket::{get, routes};
 use rocket::http::Status;
 use rocket::response::status;
+use rocket::{get, routes};
 
 #[get("/")]
 fn index() -> &'static str {
@@ -12,9 +12,16 @@ fn error() -> status::Custom<&'static str> {
     status::Custom(Status::InternalServerError, "")
 }
 
+#[get("/1/<num1>/<num2>")]
+fn cube_the_bits(num1: i32, num2: i32) -> String {
+    let a = num1 ^ num2;
+    let a = a.pow(3);
+    a.to_string()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index, error]);
+    let rocket = rocket::build().mount("/", routes![index, error, cube_the_bits]);
 
     Ok(rocket.into())
 }
