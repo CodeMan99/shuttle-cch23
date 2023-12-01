bd25f3b9c0f21fcdb184ae440995b59829664d30
Cody Taylor <codemister99@yahoo.com>
2023-12-01 07:38:54 -0500

    Task 1-2.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use rocket::http::uri::{fmt, Segments};
 use rocket::http::Status;
+use rocket::request::FromSegments;
 use rocket::response::status;
 use rocket::{get, routes};
 
@@ -12,16 +14,40 @@ fn error() -> status::Custom<&'static str> {
     status::Custom(Status::InternalServerError, "")
 }
 
-#[get("/1/<num1>/<num2>")]
+#[get("/1/<num1>/<num2>", rank = 1)]
 fn cube_the_bits(num1: i32, num2: i32) -> String {
     let a = num1 ^ num2;
     let a = a.pow(3);
     a.to_string()
 }
 
+#[derive(Debug)]
+struct SledIdInput(Vec<i32>);
+
+impl<'r> FromSegments<'r> for SledIdInput {
+    type Error = std::num::ParseIntError;
+
+    fn from_segments(segments: Segments<'r, fmt::Path>) -> Result<Self, Self::Error> {
+        let mut nums = Vec::new();
+        for segment in segments {
+            let num: i32 = segment.parse()?;
+            nums.push(num);
+        }
+        Ok(SledIdInput(nums))
+    }
+}
+
+#[get("/1/<nums..>", rank = 2)]
+fn sled_id(nums: SledIdInput) -> String {
+    let SledIdInput(nums) = nums;
+    let a = nums.iter().fold(0, |acc, &x| acc ^ x) as i64;
+    let a = a.pow(3);
+    a.to_string()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index, error, cube_the_bits]);
+    let rocket = rocket::build().mount("/", routes![index, error, cube_the_bits, sled_id]);
 
     Ok(rocket.into())
 }
