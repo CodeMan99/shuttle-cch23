2de4ed6b1d059086fc836a83e98105c9326ccf69
Cody Taylor <codemister99@yahoo.com>
2023-12-01 12:05:34 -0500

    Make parsing multiple segments generic.
    - Add rstest and use test case macro.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::rc::Rc;
+use std::str::FromStr;
+
 use rocket::http::uri::{fmt, Segments};
 use rocket::http::Status;
 use rocket::request::FromSegments;
@@ -15,25 +18,25 @@ fn error() -> status::Custom<&'static str> {
 }
 
 #[derive(Debug)]
-struct SledIdInput(Vec<i32>);
+struct SegmentsRest<T>(Rc<[T]>);
 
-impl<'r> FromSegments<'r> for SledIdInput {
-    type Error = std::num::ParseIntError;
+impl<'r, T: FromStr<Err = Err>, Err: std::fmt::Debug> FromSegments<'r> for SegmentsRest<T> {
+    type Error = Err;
 
     fn from_segments(segments: Segments<'r, fmt::Path>) -> Result<Self, Self::Error> {
-        let mut nums = Vec::new();
+        let mut values: Vec<T> = Vec::new();
         for segment in segments {
-            let num: i32 = segment.parse()?;
-            nums.push(num);
+            let value: T = segment.parse()?;
+            values.push(value);
         }
-        Ok(SledIdInput(nums))
+        Ok(SegmentsRest(values.into()))
     }
 }
 
 /// Handles both Task 1-1 & 1-2.
 #[get("/1/<nums..>")]
-fn sled_id(nums: SledIdInput) -> String {
-    let SledIdInput(nums) = nums;
+fn sled_id(nums: SegmentsRest<i32>) -> String {
+    let SegmentsRest(nums) = nums;
     let a = nums.iter().fold(0, |acc, &x| acc ^ x) as i64;
     let a = a.pow(3);
     a.to_string()
@@ -49,31 +52,17 @@ async fn main() -> shuttle_rocket::ShuttleRocket {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::*;
 
-    #[test]
-    fn test_sled_id_1() {
-        let values = vec![4, 8];
-        let segments = SledIdInput(values.into());
+    #[rstest]
+    #[case(vec![4, 8], "1728")]
+    #[case(vec![10], "1000")]
+    #[case(vec![4, 5, 8, 10], "27")]
+    #[case(vec![1, 2, 4, 8], "3375")]
+    fn test_sled_id(#[case] values: Vec<i32>, #[case] expected: &str) {
+        let segments = SegmentsRest(values.into());
         let result = sled_id(segments);
 
-        assert_eq!(result, "1728");
-    }
-
-    #[test]
-    fn test_sled_id_2() {
-        let values = vec![10];
-        let segments = SledIdInput(values.into());
-        let result = sled_id(segments);
-
-        assert_eq!(result, "1000");
-    }
-
-    #[test]
-    fn test_sled_id_3() {
-        let values = vec![4, 5, 8, 10];
-        let segments = SledIdInput(values.into());
-        let result = sled_id(segments);
-
-        assert_eq!(result, "27");
+        assert_eq!(result, expected);
     }
 }
