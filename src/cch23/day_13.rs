7add8f182b78e4f103e2df3d2865b6673d314807
Cody Taylor <codemister99@yahoo.com>
2023-12-14 07:45:36 -0500

    Add server error macro.

--- src/cch23/day_13.rs
+++ src/cch23/day_13.rs
@@ -5,6 +5,12 @@ use rocket::serde::{json::Json, Deserialize, Serialize};
 use rocket::{get, post, State};
 use sqlx::{FromRow, PgPool, QueryBuilder};
 
+macro_rules! server_err {
+    ($err:expr) => {
+        (Status::InternalServerError, $err.to_string())
+    };
+}
+
 #[repr(transparent)]
 pub struct GiftDatabase {
     pool: PgPool,
@@ -24,7 +30,7 @@ async fn sql(gift_db: &State<GiftDatabase>) -> Result<String, (Status, String)> 
     let example: Example = sqlx::query_as("SELECT 20231213")
         .fetch_one(&gift_db.pool)
         .await
-        .map_err(|err| (Status::InternalServerError, err.to_string()))?;
+        .map_err(|err| server_err!(err))?;
     Ok(example.to_string())
 }
 
@@ -40,11 +46,11 @@ struct Order {
 async fn reset(gift_db: &State<GiftDatabase>) -> Result<(), (Status, String)> {
     let mut transaction = (gift_db.pool.begin())
         .await
-        .map_err(|err| (Status::InternalServerError, err.to_string()))?;
+        .map_err(|err| server_err!(err))?;
     let _result = sqlx::query("DROP TABLE IF EXISTS orders")
         .execute(&mut *transaction)
         .await
-        .map_err(|err| (Status::InternalServerError, err.to_string()))?;
+        .map_err(|err| server_err!(err))?;
     let _result = sqlx::query(
         r#"CREATE TABLE orders (
             id INT PRIMARY KEY,
@@ -55,12 +61,12 @@ async fn reset(gift_db: &State<GiftDatabase>) -> Result<(), (Status, String)> {
     )
     .execute(&mut *transaction)
     .await
-    .map_err(|err| (Status::InternalServerError, err.to_string()))?;
+    .map_err(|err| server_err!(err))?;
 
     transaction
         .commit()
         .await
-        .map_err(|err| (Status::InternalServerError, err.to_string()))?;
+        .map_err(|err| server_err!(err))?;
 
     Ok(())
 }
@@ -81,7 +87,7 @@ async fn orders(
         .build()
         .execute(&gift_db.pool)
         .await
-        .map_err(|err| (Status::InternalServerError, err.to_string()))?;
+        .map_err(|err| server_err!(err))?;
     Ok(())
 }
 
@@ -95,7 +101,7 @@ async fn total(gift_db: &State<GiftDatabase>) -> Result<Json<OrderTotal>, (Statu
     let order_total: OrderTotal = sqlx::query_as("SELECT SUM(quantity) AS total FROM orders")
         .fetch_one(&gift_db.pool)
         .await
-        .map_err(|err| (Status::InternalServerError, err.to_string()))?;
+        .map_err(|err| server_err!(err))?;
 
     Ok(Json(order_total))
 }
@@ -118,7 +124,7 @@ async fn popular(gift_db: &State<GiftDatabase>) -> Result<Json<PopularGift>, (St
     )
     .fetch_optional(&gift_db.pool)
     .await
-    .map_err(|err| (Status::InternalServerError, err.to_string()))?;
+    .map_err(|err| server_err!(err))?;
 
     Ok(Json(popular_gift.unwrap_or_default()))
 }
