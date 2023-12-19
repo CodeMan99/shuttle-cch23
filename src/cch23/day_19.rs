feda0da402ff769f4e32dc15ca3fecadfd96ab1c
Cody Taylor <codemister99@yahoo.com>
2023-12-19 12:39:23 -0500

    day_19: Part One Complete.

--- src/cch23/day_19.rs
+++ src/cch23/day_19.rs
@@ -1,8 +1,51 @@
-use rocket::get;
+use std::str::FromStr;
 
-#[get("/")]
-fn day_19_index() {}
+use rocket::get;
+use rocket_ws::{Message, Stream, WebSocket};
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum GameState {
+    Connected,
+    Started,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum GameMessage {
+    Serve,
+    Ping,
+}
+
+struct GameMessageUnreconized;
+
+impl FromStr for GameMessage {
+    type Err = GameMessageUnreconized;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "serve" => Ok(GameMessage::Serve),
+            "ping" => Ok(GameMessage::Ping),
+            _ => Err(GameMessageUnreconized),
+        }
+    }
+}
+
+#[get("/ws/ping")]
+fn ws_ping(ws: WebSocket) -> Stream!['static] {
+    Stream! { ws =>
+        let mut game_state = GameState::Connected;
+        for await message in ws {
+            if let Message::Text(text) = message? {
+                match text.parse() {
+                    Ok(GameMessage::Serve) => game_state = GameState::Started,
+                    Ok(GameMessage::Ping) if game_state == GameState::Started => {
+                        yield "pong".into();
+                    },
+                    _ => (),
+                }
+            }
+        }
+    }
+}
 
 pub fn routes() -> Vec<rocket::Route> {
-    rocket::routes![day_19_index]
+    rocket::routes![ws_ping]
 }
