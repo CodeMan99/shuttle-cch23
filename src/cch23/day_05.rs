4c03740af98a8b841b5006135c3d64e059b5286e
Cody Taylor <codemister99@yahoo.com>
2023-12-21 08:42:16 -0500

    day_05: Complete!

--- src/cch23/day_05.rs
+++ src/cch23/day_05.rs
@@ -1,8 +1,23 @@
-use rocket::get;
+use rocket::post;
+use rocket::serde::json::Json;
 
-#[get("/")]
-fn day_05_index() {}
+#[post("/?<offset>&<limit>&<split>", data = "<names>")]
+fn name_pager(
+    offset: Option<usize>,
+    limit: Option<usize>,
+    split: Option<usize>,
+    names: Json<Vec<String>>,
+) -> Json<serde_json::Value> {
+    let offset = offset.unwrap_or_default();
+    let limit = limit.unwrap_or(usize::MAX);
+
+    if let Some(split) = split {
+        Json(names[offset..].chunks(split).take(limit).collect())
+    } else {
+        Json(names.iter().skip(offset).take(limit).cloned().collect())
+    }
+}
 
 pub fn routes() -> Vec<rocket::Route> {
-    rocket::routes![day_05_index]
+    rocket::routes![name_pager]
 }
